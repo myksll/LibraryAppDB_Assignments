91c64e3eb9b2ed4653ae8080c47fb1e4059eb13f
mkysll <mervefeyzakaraca@gmail.com>
2023-08-25 16:19:27 +0200

    CukesRunner added

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -10,7 +10,8 @@ import org.junit.runner.RunWith;
         plugin = {
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt" ,
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"
         },
         features = "src/test/resources/features" ,
         glue = "com/library/steps",
